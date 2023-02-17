88f34a3d25913837f911e547d7cf9c73ced09394
Tunde <omojuwababatunde1@gmail.com>
2023-02-17 02:41:32 -0800

    Updated task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- *  main -Entry point
+/**
+ *  main -Print a line of code with puts
  *
- *  Return: Always 0 (Success)
+ *  Return:0
  */
 int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
