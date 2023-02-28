c3f85d266ed37b09e7f6e34ff2b1c33a10b07666
Tunde <omojuwababatunde1@gmail.com>
2023-02-27 17:28:16 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 /**
  * main - check the code
- *
+ * @s: char to check
+ * Description: this will return the length of a string
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
 int a;
-for(a=0; a<*s++; a++)
+for (a = 0; a < *s++; a++)
 {
-return a;
+return (a);
 }
 
