5e895c58667fe535abaca0d055f523064cf55511
Tunde <omojuwababatunde1@gmail.com>
2023-03-31 06:50:49 -0700

    Task 1 updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,20 +2,51 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Allocates memory using malloc.
- * @b: number of memory bytes to allocate
+ * string_nconcat - concatenates two strings with a given number of bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 to copy into s1
  *
- * Return: pointer to the allocated memory or exit normal with statis 98
+ * Return: Null if error or char pointer to the new joined place in memory
  */
 
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *memory;
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+	char *x;
 
-	memory = malloc(b);
+	if (s1 == NULL)
+		a = 0;
+	else
+	{
+		for (a = 0; s1[a]; a++)
+			;
+	}
 
-	if (memory == NULL)
-		exit(98);
+	if (s2 == NULL)
+		b = 0;
+	else
+	{
+		for (b = 0; s2[b]; b++)
+			;
+	}
 
-	return (memory);
+	if (b > n)
+		b = n;
+	x = malloc(sizeof(char) * (a + b + 1));
+
+	if (x == NULL)
+		return (NULL);
+
+	for (c = 0; c < a; c++)
+		x[c] = s1[c];
+
+	for (c = 0; c < b; c++)
+		x[c + a] = s2[c];
+
+	x[a + b] = '\0';
+
+	return (x);
 }
