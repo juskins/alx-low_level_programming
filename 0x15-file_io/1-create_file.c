ae0d76c7abdced426ff4660c0f73b61aed2d7064
Tunde <omojuwababatunde1@gmail.com>
2023-04-14 18:05:34 -0700

    updated file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length]; length++)
-			;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
 	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
