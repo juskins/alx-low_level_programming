4a39ee95f5f60075a31e4423e66986493b8e13ac
tunde <omojuwababatunde1@gmail.com>
2023-05-04 19:05:45 -0700

    pushing task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * print_binary - Function prints binary rep of a number
+ * @n: the binary number to print
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1); /* Recursively go through the function */
+	}
+	_putchar((n & 1) + '0');
+}
+
